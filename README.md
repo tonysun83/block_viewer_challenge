f9e6bd2c14bdc35c89b3148d5dd74391c68d8c75
Tony Sun <tony.sun427@gmail.com>
2020-10-17 09:27:39 -0700

    add search capability

--- README.md
+++ README.md
@@ -5,6 +5,18 @@ Polkadot Blockchain Information
 ## Setup
 $ yarn add @polkadot/api
 
-
 ## Run the app
+
+### Get the latest block
+
 $ node block.js
+
+### Search by Hash
+
+$ node block.js <hash_number>
+$ node block.js 0xef6a4284d922b6565345e913a3032ea344fc56d20c947d3bb7467cf407e8107a
+
+### Search by Block Number
+
+$ node block.js <block_number>
+$ node block.js 88888
--- block.js
+++ block.js
@@ -5,14 +5,45 @@ async function main () {
   // Initialise the provider to connect to the remote polkadot node
   const provider = new WsProvider('wss://rpc.polkadot.io');
 
+  // get command line arguments
+  const args = process.argv.slice(2);
+
   // Create the API and wait until ready
   const api = await ApiPromise.create({ provider });
-  
   const chain = await api.rpc.system.chain();
-  const next_block = await api.rpc.chain.getBlock();
 
+  console.log(`Command Line Argument: ${args}`)
   console.log(`BlockChain Info: ${chain}`)
-  console.log(`Latest Block Info: ${next_block}`)
+
+  if(args.length === 0) {
+    await getNextBlock(api);
+  }
+  else if(args.length === 1) {
+    const search_arg = args[0];
+    if(/^0x/.test(search_arg)) {
+      await searchHash(api, search_arg);
+    }
+    else {
+      await searchBlockNumber(api, search_arg);
+    }
+  }
 }
 
 main().catch(console.error).finally(() => process.exit());
+
+
+async function getNextBlock(api) {
+    const response_content = await api.rpc.chain.getBlock();
+    console.log(`Latest block information: ${response_content}`)
+}
+
+async function searchBlockNumber(api, blockNumber) {
+    const blockHash = await api.rpc.chain.getBlockHash(blockNumber);
+    const block = await api.rpc.chain.getBlock(blockHash);
+    console.log(`Block ${blockNumber} info: ${block}`);
+}
+
+async function searchHash(api, blockHash) {
+    const block = await api.rpc.chain.getBlock(blockHash);
+    console.log(`Block ${blockHash} info: ${block}`);
+}
